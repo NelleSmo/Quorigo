f0e70b6e3d36fa4b3d01ed2aad325f80d6d409ad
Charnelle <nellesmo@gmail.com>
2014-03-26 21:22:06 -0400

    includes nfc code.....not fully tested yet

--- Quori/src/com/example/quori/MainActivity.java
+++ Quori/src/com/example/quori/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.quori;
 
 import java.util.Locale;
 
+import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -34,6 +45,11 @@ public class MainActivity extends FragmentActivity {
 	 */
 	ViewPager mViewPager;
 
+	private static final String TAG = "NFCReadTag";  
+    private NfcAdapter mNfcAdapter;  
+   // private IntentFilter[] mNdefExchangeFilters;  
+    private PendingIntent mNfcPendingIntent;  
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -47,7 +63,13 @@ public class MainActivity extends FragmentActivity {
 		// Set up the ViewPager with the sections adapter.
 		mViewPager = (ViewPager) findViewById(R.id.pager);
 		mViewPager.setAdapter(mSectionsPagerAdapter);
+		
+		//NFC Stuff
+		mNfcAdapter = NfcAdapter.getDefaultAdapter(this);  
 
+        
+        //NfcAdapter.NfcAdapter.ACTION_NDEF_DISCOVERED can be used see notes on android site
+        //IntentFilter scanner = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
 	}
 
 	@Override
@@ -56,6 +78,48 @@ public class MainActivity extends FragmentActivity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+//
+    protected void onResume() {  
+        super.onResume();  
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, this.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        mNfcPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,  
+                getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP  
+                | Intent.FLAG_ACTIVITY_CLEAR_TOP), 0);  
+      } 
+    protected void onNewIntent(Intent intent) {
+    	  String action = intent.getAction();
+    	  if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)){
+    	    Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+    	    NdefMessage[] messages;
+    	    if (rawMsgs != null) {
+    	      messages = new NdefMessage[rawMsgs.length];
+    	      for (int i = 0; i < rawMsgs.length; i++) {
+    	        messages[i] = (NdefMessage) rawMsgs[i];     
+    	        // To get a NdefRecord and its different properties from a NdefMesssage   
+    	     NdefRecord record = messages[i].getRecords()[i];
+    	     byte[] id = record.getId();
+    	     short tnf = record.getTnf();
+    	     byte[] type = record.getType();
+    	     String message = getTextData(record.getPayload());
+    	      }
+    	  
+    	    }
+    	  }
+    	}
+ // Decoding a payload containing text
+    private String getTextData(byte[] payload) {
+      if(payload == null) 
+        return null;
+      try {
+        String encoding = ((payload[0] & 0200) == 0) ? "UTF-8" : "UTF-16";
+        int langageCodeLength = payload[0] & 0077;
+        return new String(payload, langageCodeLength + 1, payload.length - langageCodeLength - 1, encoding);     
+      } catch(Exception e) {
+        e.printStackTrace();
+      }
+      return null;
+    }
+//   
 
 	/**
 	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -70,13 +134,36 @@ public class MainActivity extends FragmentActivity {
 		@Override
 		public Fragment getItem(int position) {
 			// getItem is called to instantiate the fragment for the given page.
-			// Return a MainSectionFragment (defined as a static inner class
+			// Return a StudentRosterFragment (defined as a static inner class
 			// below) with the page number as its lone argument.
-			Fragment fragment = new MainSectionFragment();
+			Fragment fragment;
 			Bundle args = new Bundle();
-			args.putInt(MainSectionFragment.ARG_SECTION_NUMBER, position + 1);
-			fragment.setArguments(args);
-			return fragment;
+			
+			switch(position){
+			case 0:
+				fragment = new CheckInStudentFragment();
+				args.putInt(StudentRosterFragment.ARG_SECTION_NUMBER, position + 1);
+				fragment.setArguments(args);
+				return fragment;
+			//	break;
+			case 1:
+				fragment = new StudentRosterFragment();
+				args.putInt(StudentRosterFragment.ARG_SECTION_NUMBER, position + 1);
+				fragment.setArguments(args);
+				return fragment;
+				//break;
+			case 2:
+				fragment = new RegisterStudentFragment();
+				args.putInt(StudentRosterFragment.ARG_SECTION_NUMBER, position + 1);
+				fragment.setArguments(args);
+				return fragment;
+				//break;
+			}
+			
+			return null;
+			
+			
+			
 		}
 
 		@Override
@@ -99,28 +186,27 @@ public class MainActivity extends FragmentActivity {
 			return null;
 		}
 	}
+
+}
+
+class CheckInStudentFragment extends Fragment {
 	/**
-	 * A dummy fragment representing a section of the app, but that simply
-	 * displays dummy text.
+	 * The fragment argument representing the section number for this
+	 * fragment.
 	 */
-	public static class CheckInStudentFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		public static final String ARG_SECTION_NUMBER = "section_number";
+	public static final String ARG_SECTION_NUMBER = "section_number";
 
-		public CheckInStudentFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.check_attendance_layout,
-					container, false);
-			
-
-			return rootView;
-		}
+	public CheckInStudentFragment() {
 	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.check_attendance_layout,
+				container, false);
+		
+
+		return rootView;
+	}
+	
 }
