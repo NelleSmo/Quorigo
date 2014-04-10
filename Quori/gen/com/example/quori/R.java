bc4713f970e6030fe09e03004c616e1c00c17963
devinscott20 <devin.scott20@gmail.com>
2014-04-10 18:51:58 -0400

    commit with login

--- Quori/gen/com/example/quori/R.java
+++ Quori/gen/com/example/quori/R.java
@@ -24,32 +24,33 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LastAttendedLabelTextView=0x7f080016;
+        public static final int LastAttendedLabelTextView=0x7f080017;
         public static final int RelativeLayout1=0x7f08000a;
         public static final int Space1=0x7f08000b;
         public static final int Space2=0x7f08000c;
         public static final int Space3=0x7f08000d;
         public static final int Space4=0x7f08000e;
         public static final int Space5=0x7f08000f;
-        public static final int TextView01=0x7f080018;
-        public static final int action_settings=0x7f080019;
+        public static final int TextView01=0x7f080019;
+        public static final int action_settings=0x7f08001a;
         public static final int cancel=0x7f080003;
-        public static final int currAbsLayoutTextView=0x7f080015;
-        public static final int currentAbsencesTextView=0x7f080017;
+        public static final int checkinbtn=0x7f080012;
+        public static final int currAbsLayoutTextView=0x7f080016;
+        public static final int currentAbsencesTextView=0x7f080018;
         public static final int display=0x7f080011;
         public static final int editpass=0x7f080000;
         public static final int edituser=0x7f080001;
-        public static final int emailtext=0x7f080008;
         public static final int error=0x7f080004;
-        public static final int imageButton1=0x7f080007;
+        public static final int imageButton1=0x7f080009;
         public static final int pager=0x7f080005;
         public static final int pager_title_strip=0x7f080006;
-        public static final int quickContactBadge1=0x7f080013;
-        public static final int section_label=0x7f080012;
+        public static final int quickContactBadge1=0x7f080014;
+        public static final int registerbtn=0x7f080008;
+        public static final int section_label=0x7f080013;
         public static final int sp_nameTextView=0x7f080010;
-        public static final int submitbtn=0x7f080002;
-        public static final int submitbtnpic=0x7f080009;
-        public static final int textView3=0x7f080014;
+        public static final int studentuser=0x7f080007;
+        public static final int submit=0x7f080002;
+        public static final int textView3=0x7f080015;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
